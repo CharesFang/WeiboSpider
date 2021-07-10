29a8763aec3bbb4131440e5571720e144933b86a
Funs Chares <CharesFang@users.noreply.github.com>
2021-07-10 21:54:00 +0800

    add new feature in init.sh to create a mongodb docker

--- init.sh
+++ init.sh
@@ -2,41 +2,90 @@
 
 # init constant variables
 
+# local machine mongodb dir configures
+readonly root_dir="$HOME/mongo"
+readonly data_path="$root_dir/data"
+readonly config_path="$root_dir/config"
+readonly log_path="$root_dir/log"
+
 # mongodb config file
-config_name="mongod.conf"
+readonly config_name="mongod.conf"
 
 # mongodb log file
-log_name="mongod.log"
+readonly log_name="mongod.log"
 
-# local machine mongodb dir configures
-root_dir="$HOME/mongo"
-data_path="$root_dir/data"
-config_path="$root_dir/config"
-log_path="$root_dir/log"
+# mongodb configure file path
+readonly config_file_path="$config_path/$config_name"
 
+# mongodb log file path
+readonly log_file_path="$log_path/$log_name"
 
 # to initial mongo db dir
 function init_dir() {
   if [ ! -d "$root_dir" ]; then
-  mkdir "$root_dir"
-  mkdir "$data_path"
-  mkdir "$config_path"
-  mkdir "$log_path"
+    mkdir "$root_dir"
+    mkdir "$data_path"
+    mkdir "$config_path"
+    mkdir "$log_path"
 fi
 }
 
 
 function init_config_file() {
-  config_file_path="$config_path/$config_name"
-  if [ -e "$config_file_path" ]; then
-      rm "$config_file_path"
-  fi
+# create config file
   touch "$config_file_path"
-  cat _EOF_ > "$config_file_path"
 
-  _EOF_
+# create log file
+#  touch "$log_file_path"
+
+
+# write configures
+
+  cat <<- EOF > "$config_file_path"
+#    processManagement:
+#       fork: true
+#    net:
+#       bindIp: localhost
+#       port: 27017
+#    storage:
+#       dbPath: /data/db
+#    systemLog:
+#       destination: file
+#       path: "/data/log"
+#       logAppend: true
+#    storage:
+#       journal:
+#          enabled: true
+EOF
+}
+
+function clean() {
+    rm -r "$root_dir"
 }
 
 
+function create_container() {
+  sudo docker pull mongo:4.2
+  sudo docker run --name weibo --privileged  --restart=always \
+  -p 27017:27017 \
+  -v $data_path:/data/db \
+  -v $log_path:/data/log \
+  -v $config_path:/etc/mongo \
+  -d mongo:4.2 -f /etc/mongod.conf --auth
+}
 
+# entry point of program
+function main() {
+    if [ -d "$root_dir" ]; then
+        sudo rm -r "$root_dir"
+    fi
+
+    init_dir
+
+    init_config_file
+
+    create_container
+}
+
+main
 
