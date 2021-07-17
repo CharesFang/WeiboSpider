c6e99529c97118047f719e2ef1089f7a25bf69a8
Funs Chares <CharesFang@users.noreply.github.com>
2021-07-17 14:20:25 +0800

    temporarily saved changes in user_info_spider.py

--- WeiboSpider/settings.py
+++ WeiboSpider/settings.py
@@ -23,7 +23,7 @@ LOG_LEVEL = "INFO"
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
--- WeiboSpider/spiders/_spider/user_info_spider.py
+++ WeiboSpider/spiders/_spider/user_info_spider.py
@@ -14,13 +14,19 @@ class UserInfoSpider(Spider):
     allowed_domains = ['m.weibo.cn', "weibo.com", "weibo.cn"]
 
     def __init__(self, uid: str, *args, **kwargs):
+        """"
+            The user_info_spider will get target uids
+            from cmd line argument 'uid' and each uid
+            was separated by a vertical line '|'.
+        """
         super(UserInfoSpider, self).__init__(*args, **kwargs)
         self.__generator = UserInfoConfig()
+        self.uid_list = uid.split('|')
 
     def start_requests(self):
         # how to get target uid? This is a question. Sleep!
-        for _ in self.uid.split('|'):
-            print(_)
+        for uid in self.uid_list:
+            url = self.__generator.gen_url(uid)
 
     def parse(self, response, **kwargs):
         pass
