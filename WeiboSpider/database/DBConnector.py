0a765f164c256ceb3682ac48bf4989fa4ce1758b
Funs Chares <CharesFang@users.noreply.github.com>
2021-07-17 14:51:05 +0800

    create DBConnector.py

--- WeiboSpider/database/DBConnector.py
+++ WeiboSpider/database/DBConnector.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 # @Author  : CharesFuns
-# @Time    : 2020/6/21 0:38
-# @Function: 
+# @Time    : 2021/7/17 14:46
+# @Function:
 
 import pymongo
 
 
 class DBConnector:
     def __init__(self):
-        # 重写该类或者填充本地数据库配置信息
-        self.mongo_uri = ""
-        self.mongo_database = ""
-        self.mongo_user_name = ''
-        self.mongo_pass_wd = ""
+        self.mongo_uri = "127.0.0.1"
+        self.mongo_database = "weibo"
+        self.mongo_user_name = "weibo"
+        self.mongo_pass_wd = "123456"
 
-    def create_mongo_connection(self):
+    def connect(self):
         client = pymongo.MongoClient(self.mongo_uri)
         database = client[self.mongo_database]
         database.authenticate(self.mongo_user_name, self.mongo_pass_wd)
