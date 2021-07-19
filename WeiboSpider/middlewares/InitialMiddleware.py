a58c23000c43431e107934e982fca8e0417dded2
Funs Chares <CharesFang@users.noreply.github.com>
2021-07-19 16:46:07 +0800

    optimized InitialMiddleware.py

--- WeiboSpider/middlewares/InitialMiddleware.py
+++ WeiboSpider/middlewares/InitialMiddleware.py
@@ -4,15 +4,7 @@
 # @Function:
 
 class InitialMiddleware(object):
-    def __init__(self, max_retry_times):
-        self.max_retry_times = max_retry_times
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        retry_times = crawler.settings.get('MAX_RETRY_TIME')
-        return cls(max_retry_times=retry_times)
 
     def process_request(self, request, spider):
-        request.meta['max_retry_times'] = self.max_retry_times
         request.meta['retried_times'] = 0
         return None
