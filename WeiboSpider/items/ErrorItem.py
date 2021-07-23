f5073efff3f341d1fb33cdc800f3b2bea77844c2
Funs Chares <CharesFang@users.noreply.github.com>
2021-07-23 15:44:39 +0800

    add ErrorItem.py framework

---
+++ WeiboSpider/items/ErrorItem.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author  : CharesFuns
+# @Time    : 2021/7/23 15:41
+# @Function:
+
+from scrapy import Item, Field
+
+
+class ErrorItem(Item):
+    pass
--- WeiboSpider/items/__init__.py
+++ WeiboSpider/items/__init__.py
@@ -4,7 +4,8 @@
 # @Function:
 
 from WeiboSpider.items.TweetItem import TweetItem
+from WeiboSpider.items.ErrorItem import ErrorItem
 from WeiboSpider.items.UserInfoItem import UserInfoItem
 from WeiboSpider.items.LongtextItem import LongtextItem
 
-__all__ = ['TweetItem', 'UserInfoItem', "LongtextItem"]
+__all__ = ['TweetItem', 'ErrorItem', 'UserInfoItem', "LongtextItem"]
